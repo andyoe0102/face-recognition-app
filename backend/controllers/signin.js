5837db1be29ec54f3c4b282b189f4dee94fe656e
Andy-Oe <oe.andy0102@gmail.com>
2019-12-06 15:07:57 -0600

    added JWT tokens

--- backend/controllers/signin.js
+++ backend/controllers/signin.js
@@ -1,29 +1,57 @@
-const handleSignin = (req, res,db,bcrypt) => {
+const jwt = require('jsonwebtoken');
+
+const handleSignin = (db,bcrypt,req,res) => {
     const {email, password} = req.body;
     if (!email || !password) {
-        return res.status(400).json('incorrect form submission');
+        return Promise.reject('incorrect from submission');
     }
 
-    db.select('email', 'hash').from('login')
+    return db.select('email', 'hash').from('login')
         .where('email', '=', email)
         .then(data => {
             const isValid = bcrypt.compareSync(password, data[0].hash);
             if (isValid) {
                 return db.select('*').from('users')
                     .where('email', '=', email)
-                    .then(user => {
-                        res.json(user[0])
-                    })
-                    .catch(err => res.status(400).json('unable to get user'))
+                    .then(user => user[0])
+                    .catch(err => Promise.reject('unable to get user'))
             } else {
-
-                res.status(400).json('wrong credentials')
+                Promise.reject('wrong credentials')
             }
         })
         .catch(err => res.status(400).json('wrong credentials'))
 }
 
-module.exports ={
-    handleSignin: handleSignin
+const getAuthTokenId = () =>{
+    console.log("auth ok");
+}
 
+const signToken = (email) =>{
+    const jwtPayload = {email};
+    return jwt.sign({jwtPayload}, 'JWT_SECERT', {expiresIn: '1 days'});
+}
+
+
+const createSessions = (user) =>{
+    const {email,id} = user;
+    const token = signToken(email);
+    return {success: 'true', userId: id, token}
+
+}
+
+
+const signinAuthentication = (db,bcrypt) => (req,res) =>{
+    const { authorization } = req.headers;
+    return authorization? getAuthTokenId(): 
+        handleSignin(db,bcrypt,req,res)
+            .then(data => {
+                return data.id && data.email ? createSessions(data): Promise.reject(data)
+            })
+            .then(session => res.json(session))
+            .catch(err => res.status(400).json(err));
+}
+
+module.exports ={
+    handleSignin: handleSignin,
+    signinAuthentication:signinAuthentication
 }
--- backend/server.js
+++ backend/server.js
@@ -14,13 +14,6 @@ const image = require('./controllers/image');
 const db = knex({
     client: "pg",
     connection: process.env.POSTGRES_URI
-//    {
-//        host: process.env.POSTGRES_HOST,
-//        user: process.env.POSTGRES_USER,
-//        password: '',
-//        password: process.env.POSTGRES_PASSWORD,
-//        database: process.env.POSTGRES_DB
-//    }
 });
 
 
@@ -32,41 +25,11 @@ app.use(cors());
 
 
 
-// const database={
-//     users: [
-//         {
-//             id: '123',
-//             name: 'John',
-//             email: 'john@gmail.com',
-//             password: 'cookies',
-//             entries: 0,
-//             joined: new Date()
-//         },
-//         {
-//             id: '124',
-//             name: 'Sally',
-//             email: 'sally@gmail.com',
-//             password: 'bananas',
-//             entries: 0,
-//             joined: new Date()
-//         }
-//     ],
-//     login:[
-//         {
-//             id:'987',
-//             hash: '',
-//             email: 'john@gmail.com'
-//         }
-//     ]
-// }
-
-
-
 //app.get('/',(req,res)=>{res.send(database.users);})
 app.get('/', (req, res) => { res.send("ITS WORKING"); })
-app.post('/signin',(req,res) => {signin.handleSignin(req,res,db,bcrypt)})
+app.post('/signin', signin.signinAuthentication(db, bcrypt))
 app.post('/register', (req,res) => {register.handleRegister(req,res,db,bcrypt) })
-app.get('/profile/:id',(req,res) => {msWriteProfilerMark.handleProfileGet(req,res,db)})
+app.get('/profile/:id',(req,res) => {profile.handleProfileGet(req,res,db)})
 app.post('/profile/:id', (req,res) => {profile.handleProfileUpdate(req,res,db)})
 app.put('/image',(req,res) => {image.handleImage(req,res,db)})
 app.post('/imageurl', (req,res) => {image.handleApiCall(req,res)})
